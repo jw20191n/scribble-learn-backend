866eded5b2e63f67e3992eba0bc5dbda15814302
jw20191n <clairejw1015@gmail.com>
2019-11-20 10:00:01 -0500

    set up

---
+++ app/controllers/students_controller.rb
@@ -0,0 +1,18 @@
+class StudentsController < ApplicationController
+
+    def index 
+        @students = Student.all
+        render json:@students
+    end
+
+    def show
+        @student = Student.find(params[:id])
+        render json:@student
+    end
+
+    def create
+        data = JSON.parse(request.raw_post)
+        @student = Student.create(data)
+        render json:@student
+    end
+end
